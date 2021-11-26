a7b49443529a333b1506e1fef8956e56288fb0ef
Rubaiya Razin Bushra <rubaiya35-2713@diu.edu.bd>
2021-11-27 01:08:00 +0600

    Header done

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+// by using rafce short cut key 
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="banner">
+        
+        <div className="banner__content">
+            <div className="container">
+                <div className="banner__text">
+                    <h3>Food Order Handling</h3>
+                    <h1>Food Made With Love</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+                        minus ut mollitia error molestiae quia.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default Header
