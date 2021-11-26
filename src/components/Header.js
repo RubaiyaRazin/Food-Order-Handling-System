2da775cd4d094232d92980de331cb74c36bc2f86
Rubaiya Razin Bushra <rubaiya35-2713@diu.edu.bd>
2021-11-27 03:59:13 +0600

    Navbar & order confirmation button done.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,26 @@
 // by using rafce short cut key 
 import React from 'react'
+import Navbar from './Navbar';
 
 const Header = () => {
     return (
         <div className="banner">
-        
+        <Navbar />
         <div className="banner__content">
             <div className="container">
                 <div className="banner__text">
                     <h3>Food Order Handling</h3>
                     <h1>Food Made With Love</h1>
                     <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
-                        minus ut mollitia error molestiae quia.
+                        To Eat Is A Necessity, But To Eat Intelligently Is An Art!
+                        Good Food Is The Foundation Of Genuine Happiness.
                     </p>
+
+                    <div className="banner__btn">
+                        <a href="" className="btn btn-smart">
+                            Confirm Order
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="navbar__text">
+                
+                <ul className="navbar__ul">
+                    <li>
+                        <a href="">Home</a>
+                    </li>
+                    <li>
+                        <a href="">Menu</a>
+                    </li>
+                    <li>
+                        <a href="">Rivew</a>
+                    </li>
+                    <li>
+                        <a href="">Order</a>
+                    </li>
+                    <li>
+                        <a href="">Contact</a>
+                    </li>
+                </ul>
+        </div>
+        </div>
+    )
+}
+
+export default Navbar
