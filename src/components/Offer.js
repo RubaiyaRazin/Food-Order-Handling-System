9427d72ea8c8602bf054c2c5ed6a90efe3156331
Kazi Rifat Ahmed <kazi35-2665@diu.edu.bd>
2021-11-29 01:10:26 +0600

    need to update the images & changes into css

--- src/components/Offer.js
+++ src/components/Offer.js
@@ -1,7 +1,7 @@
 
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
-import pic1 from "/components/img/food1.png";
+import pic1 from "../img/food1.png";
 
 import React, { Component } from "react";
 import Slider from "react-slick";
