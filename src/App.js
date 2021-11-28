9145a954db8849eebf08ace78fedd747b8a6e0a1
Kazi Rifat Ahmed <kazi35-2665@diu.edu.bd>
2021-11-29 00:30:00 +0600

    Add offer.js to App.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from "./components/Header";       // importing Header.js to App.js
-import About from './components/About';
+import About from "./components/About";
+import Offer from "./components/Offer";
 
 function App() {
   return (
     <div>
         <Header />
+        <Offer/>
         <About />
     </div>
   );
---
+++ src/components/Offer.js
@@ -0,0 +1,134 @@
+
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import pic1 from "./images/food1.png";
+
+import React, { Component } from "react";
+import Slider from "react-slick";
+
+export default class MultipleItems extends Component {
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 3
+    };
+    return (
+      <div className="offer-slide">
+        <h3> Exclusive </h3>
+        <h4>BEST OFFER</h4>
+        <Slider {...settings}>
+            <div className="box">
+            <div className="slide-img">
+                <img src={pic1} alt="food img"/>
+                <div className="overlayer">
+                    <a href="#" className="cart-btn">ADD TO CART</a>
+                </div>
+            </div>
+            <hr/>
+            <div className="detail-box">
+                <div className="type">
+                    <a href="#">Fried Potato & Drinks</a>
+                </div>
+                <a href="#" className="price">$10</a>
+            </div>
+            </div>
+
+            <div className="box">
+            <div className="slide-img">
+                <img src={pic1} alt="food img"/>
+                <div className="overlayer">
+                    <a href="#" className="cart-btn">ADD TO CART</a>
+                </div>
+            </div>
+            <hr/>
+            <div className="detail-box">
+                <div className="type">
+                    <a href="#">Fried Potato & Drinks</a>
+                </div>
+                <a href="#" className="price">$10</a>
+            </div>
+            </div>
+
+            <div className="box">
+            <div className="slide-img">
+                <img src={pic1} alt="food img"/>
+                <div className="overlayer">
+                    <a href="#" className="cart-btn">ADD TO CART</a>
+                </div>
+            </div>
+            <div className="detail-box">
+                <div className="type">
+                    <a href="#">Fried Potato & Drinks</a>
+            </div>
+                <a href="#" className="price">$10</a>
+            </div>
+            </div>
+
+            <div className="box">
+            <div className="slide-img">
+                <img src={pic1} alt="food img"/>
+                <div className="overlayer">
+                    <a href="#" className="cart-btn">ADD TO CART</a>
+                </div>
+            </div>
+            <div className="detail-box">
+                <div className="type">
+                    <a href="#">Fried Potato & Drinks</a>
+                </div>
+                <a href="#" className="price">$10</a>
+            </div>
+            </div>
+
+            <div className="box">
+            <div className="slide-img">
+                <img src={pic1} alt="food img"/>
+                <div className="overlayer">
+                    <a href="#" className="cart-btn">ADD TO CART</a>
+                </div>
+            </div>
+            <div className="detail-box">
+                <div className="type">
+                    <a href="#">Fried Potato & Drinks</a>
+                </div>
+                <a href="#" className="price">$10</a>
+            </div>
+            </div>
+
+            <div className="box">
+            <div className="slide-img">
+                <img src={pic1} alt="food img"/>
+                <div className="overlayer">
+                    <a href="#" className="cart-btn">ADD TO CART</a>
+                </div>
+            </div>
+            <div className="detail-box">
+                <div className="type">
+                    <a href="#">Fried Potato & Drinks</a>
+                </div>
+                <a href="#" className="price">$10</a>
+            </div>
+            </div>
+
+            <div className="box">
+            <div className="slide-img">
+                <img src={pic1} alt="food img"/>
+                <div className="overlayer">
+                    <a href="#" className="cart-btn">ADD TO CART</a>
+                </div>
+            </div>
+            <div className="detail-box">
+                <div className="type">
+                    <a href="#">Fried Potato & Drinks</a>
+                </div>
+                <a href="#" className="price">$10</a>
+            </div>
+            </div>
+
+        </Slider>
+      </div>
+    );
+  }
+}
