1078ba92c85686dccf02afa2e15883f0bfc3bcf3
Kazi Rifat Ahmed <kazi35-2665@diu.edu.bd>
2021-12-02 00:57:00 +0600

    CSS Modified

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,6 @@ import Navbar from './Navbar';
 const Header = () => {
     return (
         <div className="banner">
-        <Navbar />
         <div className="banner__content">
             <div className="container">
                 <div className="banner__text">
